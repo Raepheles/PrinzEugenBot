d740290d2150610d13d5193af62cc7dd2bcfb1da
Raepheles <raepheles@yahoo.com>
2021-10-15 22:18:29 +0300

    Backup data

--- src/main.ts
+++ src/main.ts
@@ -1,36 +1,53 @@
-import Cluster from './structures/Cluster';
-import { createConnection } from 'mongoose';
-import config from '../config.json';
-import { CronJob } from 'cron';
-import { readFile } from 'fs-extra';
-import { Ship, ParseData, UnreleasedShip, Equipment } from './types/ParseData';
-import { Parser } from './Parser';
-import { getLogger, configure } from 'log4js';
-import { writeFile } from 'fs-extra';
+import Cluster from "./structures/Cluster";
+import { createConnection } from "mongoose";
+import config from "../config.json";
+import { CronJob } from "cron";
+import { readFile } from "fs-extra";
+import { Ship, ParseData, UnreleasedShip, Equipment } from "./types/ParseData";
+import { Parser } from "./Parser";
+import { getLogger, configure } from "log4js";
+import { writeFile, copy } from "fs-extra";
 
 const logConfig = {
   appenders: {
-    outFilter: { type: 'stdout' },
-    debugFile: { type: 'dateFile', filename: 'log/debug.log', pattern: 'yyyy-MM-dd', keepFileExt: true, alwaysIncludePattern: true },
-    traceFile: { type: 'dateFile', filename: 'log/trace.log', pattern: 'yyyy-MM-dd', keepFileExt: true, alwaysIncludePattern: true },
-    out: { type: 'logLevelFilter', level: config.logLevel, appender: 'outFilter' },
-    debug: { type: 'logLevelFilter', level: 'debug', appender: 'debugFile' },
-    trace: { type: 'logLevelFilter', level: 'trace', appender: 'traceFile' }
+    outFilter: { type: "stdout" },
+    debugFile: {
+      type: "dateFile",
+      filename: "log/debug.log",
+      pattern: "yyyy-MM-dd",
+      keepFileExt: true,
+      alwaysIncludePattern: true,
+    },
+    traceFile: {
+      type: "dateFile",
+      filename: "log/trace.log",
+      pattern: "yyyy-MM-dd",
+      keepFileExt: true,
+      alwaysIncludePattern: true,
+    },
+    out: {
+      type: "logLevelFilter",
+      level: config.logLevel,
+      appender: "outFilter",
+    },
+    debug: { type: "logLevelFilter", level: "debug", appender: "debugFile" },
+    trace: { type: "logLevelFilter", level: "trace", appender: "traceFile" },
   },
   categories: {
     default: {
-      appenders: [ 'out', 'debug' ],
-      level: 'trace'
-    }
-  }
+      appenders: ["out", "debug"],
+      level: "trace",
+    },
+  },
 };
 
-if(config.traceFileLogsOn) logConfig.categories.default.appenders.push('trace');
+if (config.traceFileLogsOn)
+  logConfig.categories.default.appenders.push("trace");
 
 configure(logConfig);
 const logger = getLogger();
 
-start().catch(err => {
+start().catch((err) => {
   console.error(`There was an error starting the application: ${err}`);
   process.exit(2);
 });
@@ -47,8 +64,10 @@ async function start() {
     unreleasedShips = obj.unreleasedShips;
     equipments = obj.equipments;
     date = obj.date;
-  } catch(err) {
-    logger.error(`Got error while parsing the data and there is no data present. Exiting the app. Error: ${err}`);
+  } catch (err) {
+    logger.error(
+      `Got error while parsing the data and there is no data present. Exiting the app. Error: ${err}`
+    );
     process.exit(1);
   }
 
@@ -56,35 +75,48 @@ async function start() {
     useFindAndModify: false,
     useCreateIndex: true,
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
   });
 
-  const client = new Cluster(conn, logger, ships, unreleasedShips, equipments, date);
+  const client = new Cluster(
+    conn,
+    logger,
+    ships,
+    unreleasedShips,
+    equipments,
+    date
+  );
   await client.commands.init();
   await client.events.init();
   await client.notificationHandler.init();
   await client.login(client.config.token);
 
-  const cronJob = new CronJob('0 3 * * *', async () => {
-    parseData(client);
-  }, null, true, 'Europe/Istanbul');
+  const cronJob = new CronJob(
+    "0 3 * * *",
+    async () => {
+      parseData(client);
+    },
+    null,
+    true,
+    "Europe/Istanbul"
+  );
   cronJob.start();
 }
-
 
 async function loadOrParseData() {
   try {
     return await loadData();
-  } catch(err) {
+  } catch (err) {
     logger.error(`There was an error while loading data: ${err}`);
   }
   return await parseData();
 }
 
 async function loadData() {
-  const buffer = await readFile(`${config.dataFileName}.json`, 'utf8');
+  const buffer = await readFile(`${config.dataFileName}.json`, "utf8");
   const obj = JSON.parse(buffer);
-  if(!obj.date || !obj.ships || !obj.unreleasedShips || !obj.equipments) throw Error(`Invalid ${config.dataFileName}.json`);
+  if (!obj.date || !obj.ships || !obj.unreleasedShips || !obj.equipments)
+    throw Error(`Invalid ${config.dataFileName}.json`);
   const date = new Date(obj.date);
   const ships: Ship[] = obj.ships;
   const unreleasedShips: UnreleasedShip[] = obj.unreleasedShips;
@@ -93,12 +125,17 @@ async function loadData() {
     date,
     ships,
     unreleasedShips,
-    equipments
+    equipments,
   };
 }
 
 async function parseData(client?: Cluster) {
-  const parser = new Parser(logger, config.wikiBaseUrl, config.shipListUrl, config.equipmentListUrl);
+  const parser = new Parser(
+    logger,
+    config.wikiBaseUrl,
+    config.shipListUrl,
+    config.equipmentListUrl
+  );
   const data: ParseData = await parser.parse();
   const ships = data.ships;
   const date = data.date;
@@ -108,19 +145,23 @@ async function parseData(client?: Cluster) {
     date,
     ships,
     unreleasedShips,
-    equipments
+    equipments,
   };
-  if(client) {
+  if (client) {
     client.lastDataUpdate = date;
     client.ships = ships;
     client.unreleasedShips = unreleasedShips;
     client.equipments = equipments;
   }
+  await copy(
+    `${config.dataFileName}.json`,
+    `/data-backup/${config.dataFileName}-${new Date().toLocaleDateString()}`
+  );
   await writeFile(`${config.dataFileName}.json`, JSON.stringify(obj));
   return {
     date,
     ships,
     unreleasedShips,
-    equipments
+    equipments,
   };
 }
